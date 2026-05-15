ef6bc97349e9a1ec4177386f706cb0fc1e5d0133
348775859-wq <348775859@gapps.yrdsb.ca>
2026-05-15 11:10:36 -0400

    added world p1

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,46 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    public int score = 0;
+    Label scoreLabel;
+    
     public MyWorld() {
-        super(600, 400, 1);
+        
+        // create a 600x400 world
+        super(600, 400, 1, false);
+        
+        // create an Elephant object
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 300);
+        
+        // create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
+        
+        createApple();
+    }
+
+    // game over label
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
+    //increase score
+    public void increaseScore()
+    {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    
+    // create an apple object at random location
+    public void createApple()
+    {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber(600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
